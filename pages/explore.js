89bf2b22b50b8538589bcc9d3f4856f99f225e97
root <root@RedrumGuerrilla.localdomain>
2020-03-11 00:05:00 -0400

    added options to explore interface

--- pages/explore.js
+++ pages/explore.js
@@ -1,5 +1,16 @@
 import Layout from "../components/Layout";
+import "../components/explore.scss"
 
-const ExploreView = props => <Layout>{`Explore View.`}</Layout>;
+const ExploreView = props => <Layout>
+    <div className="explore-interface">
+        <h1>Welcome to the COVID-19 Hackathon:</h1>
+        <ul>
+            <li><a>Virus Information</a></li>
+            <li><a>Spread Dashboard</a></li>
+            <li><a>Prevention Equipment</a></li>
+            <li><a>About</a></li>
+        </ul>
+    </div>
+    </Layout>;
 
 export default ExploreView;
