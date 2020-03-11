a4fdd9db8dfbee81314ba5160484ce13b031c2d6
root <root@RedrumGuerrilla.localdomain>
2020-03-10 23:58:50 -0400

    readme

--- components/TwitterContainer.js
+++ components/TwitterContainer.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 
+import "./TwitterContainer.scss"
+
 const TwitterContainer = () => {
     useEffect(() => {
       const anchor = document.createElement("a");
