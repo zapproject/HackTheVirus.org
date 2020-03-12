764b4060e60d22211d6e462dbb3819e537cb7824
adammmanka <adammmanka@gmail.com>
2020-03-12 19:21:44 -0400

    button change

--- components/Button.js
+++
@@ -1,9 +0,0 @@
-import "../components/Button.scss";
-
-const Button = props => (
-
-<a href="https://discord.gg/qZG5qCm"><button className="btn draw-border" >Join The Movement!</button></a>
-
-);
-
-export default Button;
--- components/JoinDiscord.js
+++
@@ -1,5 +0,0 @@
-const JoinDiscord = props => (
-    <div className="discord"><a href="https://discord.gg/qZG5qCm">Click here to join our discord!</a></div>
-  );
-  
-  export default JoinDiscord;
