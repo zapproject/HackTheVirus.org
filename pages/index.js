db5e294eed4c43ef131c0609fc74604d15250a85
adammmanka <adammmanka@gmail.com>
2020-03-12 23:31:18 -0400

    started deleting dead files

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
-import Link from "next/link";
-
 import Layout from "../components/Layout";
-import JoinDiscord from "../components/JoinDiscord";
 import MainDiv from "../components/MainDiv";
-import Button from "../components/Button";
 import DiscordSlinky from "../components/DiscordSlinky";
 
 
@@ -11,10 +7,6 @@ const Index = () => (
 <Layout> 
     <div className="containingdiv">
         <MainDiv />
-          
-    
-    {/* <div className="discord-div"><JoinDiscord className="discord"/></div> */}
-    {/* <Button /> */}
         <DiscordSlinky />
     </div>
     
