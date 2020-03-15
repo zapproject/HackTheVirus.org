1accd2e1dd0f316cb18422f3c1be94d9c7ed7194
adammmanka <adammmanka@gmail.com>
2020-03-14 21:02:10 -0400

    update twitter feed

--- components/DiscordSlinky.js
+++ components/DiscordSlinky.js
@@ -2,7 +2,7 @@ import "../components/DiscordSlinky.scss"
 
 const DiscordSlinky = props => (
 
-<iframe class="discord" src="https://discordapp.com/widget?id=685175752538456286&theme=dark"/>
+<iframe className="discord" src="https://discordapp.com/widget?id=685175752538456286&theme=dark"/>
 
 );
 
--- components/TwitterContainer.js
+++ components/TwitterContainer.js
@@ -9,7 +9,7 @@ const TwitterContainer = () => {
       anchor.setAttribute("data-theme", "dark");
       anchor.setAttribute("data-tweet-limit", "5");
       anchor.setAttribute("data-chrome", "noheader nofooter noborders");
-      anchor.setAttribute("href", "https://twitter.com/CoronaVirusNe10");
+      anchor.setAttribute("href", "https://twitter.com/coronaviruscare");
       document.getElementsByClassName("twitter-embed")[0].appendChild(anchor);
   
       const script = document.createElement("script");
